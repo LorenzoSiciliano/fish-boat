f80309f7f322a222a3a19609b8606ef0040c983a
Daniele3 <daniele.schiavolin@edu.itspiemonte.it>
2018-01-17 19:03:04 +0100

    Aggiunta modifica dei dati dei pesci tramite input, aggiunto css, array di pesci

--- src/app/fishes/fishes.component.ts
+++ src/app/fishes/fishes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Fish } from '../fish';
+import { FISHES } from '../mock-fishes';
 import { FishService } from '../fish.service';
 
 @Component({
@@ -8,15 +9,20 @@ import { FishService } from '../fish.service';
   styleUrls: ['./fishes.component.css']
 })
 export class FishesComponent implements OnInit {
-  fishes : Fish[];
-  constructor(private fishService: FishService) { }
+  //fishes : Fish[];
+  fishes = FISHES;
+//  constructor(private fishService: FishService) { }
+  selectedFish: Fish;
 
-  getFishes(): void {
-    this.fishService.getFishes().subscribe(fishes => this.fishes = fishes);
+  onSelect(fish: Fish): void {
+    this.selectedFish = fish;
   }
+  /*getFishes(): void {
+    this.fishService.getFishes().subscribe(fishes => this.fishes = fishes);
+  }*/
 
   ngOnInit() {
-    this.getFishes();
+    //this.getFishes();
   }
 
 }
---
+++ src/app/mock-fishes.ts
@@ -0,0 +1,7 @@
+import { Fish } from './fish';
+
+export const FISHES: Fish[] = [
+  { id: 11, breed: 'Trota', length: '50', weight: '102' },
+  { id: 12, breed: 'Salmone' length: '50', weight: '111' },
+  { id: 13, breed: 'Carpa', length: '50', weight: '100' }
+];
