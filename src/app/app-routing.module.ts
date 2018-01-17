70160d070304367f9d75f533c925d47cfe09b459
LorenzoSiciliano <lorenzo.siciliano@edu.itspiemonte.it>
2018-01-17 22:24:29 +0100

    modificato dettaglio pesce per funzionare con il routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes} from '@angular/router';
 import { FishesComponent } from './fishes/fishes.component';
+import { FishDetailComponent }  from './fish-detail/fish-detail.component';
 
 const routes = [
   { path: '', redirectTo: '/fishes', pathMatch: 'full' },
+  { path: 'detail/:id', component: FishDetailComponent },
   { path: 'fishes', component: FishesComponent }
 ]
 
--- src/app/fish-detail/fish-detail.component.ts
+++ src/app/fish-detail/fish-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Fish } from '../fish';
+import { ActivatedRoute } from '@angular/router';
+import { FishService }  from '../fish.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-fish-detail',
@@ -8,9 +11,29 @@ import { Fish } from '../fish';
 })
 export class FishDetailComponent implements OnInit {
   @Input() fish: Fish;
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private fishService: FishService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.fishService.getFish(id)
+      .subscribe(fish => this.fish = fish);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.fish.img = "assets/img/"+this.fish.breed+".png";
+    this.fishService.updateFish(this.fish)
+     .subscribe(() => this.goBack());
+ }
 }
