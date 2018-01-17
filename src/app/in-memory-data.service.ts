a906be4913f634bc7157c7ba4a549211486f0766
LorenzoSiciliano <lorenzo.siciliano@edu.itspiemonte.it>
2018-01-17 21:34:52 +0100

    risolto problema immagini

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,9 +3,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const fishes = [
-      { id: 1, breed: "Carpa", length: "50 cm",weight: "10 Kg" },
-      { id: 2, breed: "Pesce gatto", length: "55 cm",weight: "3 Kg" },
-      { id: 3, breed: "Trota", length: "30 cm",weight: "5 Kg" }
+      { id: 1, breed: "Carpa", length: "50 cm",weight: "10 Kg",img: "assets/img/Carpa.png" },
+      { id: 2, breed: "Pesce gatto", length: "55 cm",weight: "3 Kg",img: "assets/img/Pesce gatto.png" },
+      { id: 3, breed: "Trota", length: "30 cm",weight: "5 Kg",img: "assets/img/Trota.png" }
     ];
     return {fishes};
   }
